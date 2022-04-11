aa7134460ed6d19ee91e0ce1ccb3f1c12c52fecb
LichiBruno <lichiii@pm.me>
2022-04-11 02:55:09 -0300

    minor changes

--- src/components/Page/styles.js
+++ src/components/Page/styles.js
@@ -6,14 +6,13 @@ const useStyles = makeStyles(theme => ({
   root: {
     height: '100%',
     position: isMobile ? 'absolute' : 'relative',
-    'margin-left': isMobile ? 'auto' : '',
-    'margin-right': isMobile ? 'auto' : '',
     left: isMobile ? '0' : '',
     right: isMobile ? '0' : '',
+    'margin-left': isMobile ? 'auto' : '',
+    'margin-right': isMobile ? 'auto' : '',
     // 'text-align': isMobile ? 'center' : '',
     'padding-left': theme.spacing(isMobile ? 1.5 : 3),
     'padding-right': theme.spacing(isMobile ? 1.5 : 3),
-  
   },
 }));
 
--- src/pages/Page1/Chapter1.js
+++ src/pages/Page1/Chapter1.js
@@ -3,19 +3,9 @@ import React from 'react';
 function Chapter1() {
     return (
       <div className="Chapters" style={{ wordBreak: "break-all"}}>
-        <h1>Contexto general</h1>
-        <p>asd</p>
-        <p>asasd12asd12asd12asd12asd12asd12asd12asd122asasd12asd12asd12asd12asd12asd12asd12asd122asasd12asd12asd12asd12asd12asd12asd12asd122asasd12asd12asd12asd12asd12asd12asd12asd122asasd12asd12asd12asd12asd12asd12asd12asd122asasd12asd12asd12asd12asd12asd12asd12asd122asasd12asd12asd12asd12asd12asd12asd12asd122asasd12asd12asd12asd12asd12asd12asd12asd122asasd12asd12asd12asd12asd12asd12asd12asd122asasd12asd12asd12asd12asd12asd12asd12asd122v</p>
-        <p>asa</p>
-        <p>asd12</p>
-        <p>asd</p>
-        <p>asd12</p>
-        <p>asd</p>
-        <p>asd12</p>
-        <p>asd</p>
-        <p>asd12</p>
-        <p>asd</p>
-        <p>asd12</p>
+        <h3>Contexto general</h3>
+        <h4>Contexto general</h4>
+        <p>rellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenoellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenoellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorellenorelleno</p>
       </div>
     );
   }
