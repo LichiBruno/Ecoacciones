5efb67713ef61719dfb6bdd904796460d3f62e36
LichiBruno <lichiii@pm.me>
2022-04-11 00:32:56 -0300

    Fixed text problem on mobile version

--- src/components/Page/styles.js
+++ src/components/Page/styles.js
@@ -5,9 +5,15 @@ import isMobile from 'utils/isMobile';
 const useStyles = makeStyles(theme => ({
   root: {
     height: '100%',
-    position: 'relative',
+    position: isMobile ? 'absolute' : 'relative',
+    'margin-left': isMobile ? 'auto' : '',
+    'margin-right': isMobile ? 'auto' : '',
+    left: isMobile ? '0' : '',
+    right: isMobile ? '0' : '',
+    // 'text-align': isMobile ? 'center' : '',
     'padding-left': theme.spacing(isMobile ? 1.5 : 3),
     'padding-right': theme.spacing(isMobile ? 1.5 : 3),
+  
   },
 }));
 
