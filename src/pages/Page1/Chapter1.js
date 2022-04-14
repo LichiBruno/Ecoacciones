ea9258ff7c88cea2a4eae7cf556352f344742196
LichiBruno <lichiii@pm.me>
2022-04-14 02:42:20 -0300

    chapter 1 finished

--- src/pages/Page1/Chapter1.js
+++ src/pages/Page1/Chapter1.js
@@ -1,39 +1,205 @@
-import React from 'react';
-import { FaRecycle as Recycle } from 'react-icons/fa';
-
+import React from "react";
+import { FaRecycle as Recycle } from "react-icons/fa";
 
 function Chapter1() {
-    return (
-      <div className='Chapters'>
-        <Recycle style={{ 
-          color: 'rgb(238, 86, 3)',
+  return (
+    <div className="Chapters">
+      <Recycle
+        style={{
+          color: "rgb(238, 86, 3)",
           // uncomment the lines below to center icon
           // position: 'relative',
           // left: '50%',
-          width: '2em',
-          height: '2em'
-      }}/>
-        <h3>Contexto general</h3>
-        <p>En este capítulo revisamos los aspectos generales del contexto global que nos atraviesa, estableciendo los interrogantes esenciales que motivan y fundamentan la creación de esta guía. Así, proponemos identificar las problemáticas como paso prioritario para iniciar una reflexión necesaria, definir objetivos y ejecutar los grandes o pequeños cambios que se requieran.</p>
-        <h3>El cambio climático</h3>
-        <h4>¿Qué relación tiene con las ciudades y la ciudadanía?</h4>
-        <p>La Organización de las Naciones Unidas (ONU) estima que en la actualidad más de la mitad de la población en el mundo vive en ciudades y que esta cifra podría alcanzar los dos tercios para el año 2030.</p>
-        <p>En América Latina el porcentaje es aún mayor, dónde el 81% de la población vive en áreas urbanas (Banco Mudnial, 2020). En estas ciudades se consume una gran cantidad de energía y se producen aproximadamente el 70% de las emisiones de gases de efecto invernadero derivadas de la energía, que absorben el calor y provocan el calentamiento de la Tierra.</p>
-        <h4>¿Cómo afecta el cambio climático a las ciudades?</h4>
-        <p>El informe "Las ciudades y el cambio climático. Orientaciones para políticas" (ONU hábitat: 2011) advierte que si bien los riesgos del cambio climático varían en función de las particularidades de las ciudades, es posible establecer algunas características comunes:</p>
-        <ol>
-          <li>El cambio climático no afecta a toda la población de una ciudad por igual, por el contrario, aspectos como género, edad y situación económica intervienen en el grado de vulnerabilidad de las personas. Es decir, que el impacto del cambio climático reforzará las desigualdades existentes.</li>
-          <li>La falta de planificación urbama y las características constructivas de las ciudades podrían ser un limitante a la adaptación ed la infraestructura urbana, con el consecuente riesgo para las personas que en ellas habitan y sus bienes. Por ello, en la planificación de las ciudades deben tenerse en cuenta las demandas actuales y futuras de recursos. También habrá de identificarse los grupos humanos más vulnerables al cambio climático</li>
-          <li>Los impactos del cambio climático como la dificultad en la presentación de servicios básicos, la afectación de bienes y servicios de los ecosistemas pueden ser de largo plazo.</li>
-        </ol>
-        <p>"El análisis de cómo los centros urbanos contribuyen al cambio climático requiere comprender cómo el transporte, los sistemas de calefacción y refrigeración, las industrias y otras actividades urbanas e infraestructuras actúan tanto como emisores como causas directas del cambio climático. Los centros urbanos crean dos categorías principales de impactos sobre el ciclo del carbono y el sistema climático, concretamente cambios relacionados con la emisión de aerosoles, GEI y residuos sólidos, y cambios relacionados con el uso del suelo". (ONU Hábitat. 2011: 16)</p>
-        <h4>¿Qué se puede hacer frente a este escenario? ¿Qué podemos hacer como habitantes de las ciudades? ¿Qué propone esta guía?</h4>
-        <p>La huella de carbono se define como el conjunto de emisiones de gases de efecto invernadero producidas, directa o indirectamente, por personas, organizaciones, productos, eventos o regiones geográficas, en términos de CO2 equivalentes. La medición de esta huella es una herramienta de gestión útil para conocer las conductas o acciones que contribuyen a aumentar nuestras emisiones, cómo podemos mejorarlas y realizar un uso más eficiente de los recursos.</p>
-        <p>Hoy las huellas de nuestras ciudades son enormes y para reducirlas es clave avanzar en su planificación, innovar en el sector energético, efectuar cambios en los modelos constructivos y en los medios que utilizaremos para transportarnos. Además, como ciudadanos y ciudadanas podemos y debemos contribuir a reducir el impacto del cambio climático</p>
-        <p>En ese sentido, la <strong>Agenda 2030 para el Desarrollo Sostenible</strong> tiene como finalidad erradicar la pobreza y el hambre en todo el mundo de aquí al 2030, combatir las desigualdades, construir sociedades pacíficas, justas e inclusivas, proteger los derechos humanos, promover la igualdad entre los géneros, a la vez se garantiza la protección duradera del planeta y sus recursos naturales.</p>
-        <p>La provincia del Neuquén, en línea con esta Agenda, promueve medidas para la protección del ambiente y la sostenibilidad en el territorio, favoreciendo a que todas las personas gocen de paz y prosperidad.</p>
-      </div>
-    );
-  }
+          width: "2em",
+          height: "2em",
+        }}
+      />
+      <h3>Contexto general</h3>
+      <p>
+        En este capítulo revisamos los aspectos generales del contexto global
+        que nos atraviesa, estableciendo los interrogantes esenciales que
+        motivan y fundamentan la creación de esta guía. Así, proponemos
+        identificar las problemáticas como paso prioritario para iniciar una
+        reflexión necesaria, definir objetivos y ejecutar los grandes o pequeños
+        cambios que se requieran.
+      </p>
+      <h3>El cambio climático</h3>
+      <h4>¿Qué relación tiene con las ciudades y la ciudadanía?</h4>
+      <p>
+        La Organización de las Naciones Unidas (ONU) estima que en la actualidad
+        más de la mitad de la población en el mundo vive en ciudades y que esta
+        cifra podría alcanzar los dos tercios para el año 2030.
+      </p>
+      <p>
+        En América Latina el porcentaje es aún mayor, dónde el 81% de la
+        población vive en áreas urbanas (Banco Mudnial, 2020). En estas ciudades
+        se consume una gran cantidad de energía y se producen aproximadamente el
+        70% de las emisiones de gases de efecto invernadero derivadas de la
+        energía, que absorben el calor y provocan el calentamiento de la Tierra.
+      </p>
+      <h4>¿Cómo afecta el cambio climático a las ciudades?</h4>
+      <p>
+        El informe "Las ciudades y el cambio climático. Orientaciones para
+        políticas" (ONU hábitat: 2011) advierte que si bien los riesgos del
+        cambio climático varían en función de las particularidades de las
+        ciudades, es posible establecer algunas características comunes:
+      </p>
+      <ol>
+        <li>
+          El cambio climático no afecta a toda la población de una ciudad por
+          igual, por el contrario, aspectos como género, edad y situación
+          económica intervienen en el grado de vulnerabilidad de las personas.
+          Es decir, que el impacto del cambio climático reforzará las
+          desigualdades existentes.
+        </li>
+        <li>
+          La falta de planificación urbama y las características constructivas
+          de las ciudades podrían ser un limitante a la adaptación ed la
+          infraestructura urbana, con el consecuente riesgo para las personas
+          que en ellas habitan y sus bienes. Por ello, en la planificación de
+          las ciudades deben tenerse en cuenta las demandas actuales y futuras
+          de recursos. También habrá de identificarse los grupos humanos más
+          vulnerables al cambio climático
+        </li>
+        <li>
+          Los impactos del cambio climático como la dificultad en la
+          presentación de servicios básicos, la afectación de bienes y servicios
+          de los ecosistemas pueden ser de largo plazo.
+        </li>
+      </ol>
+      <p>
+        "El análisis de cómo los centros urbanos contribuyen al cambio climático
+        requiere comprender cómo el transporte, los sistemas de calefacción y
+        refrigeración, las industrias y otras actividades urbanas e
+        infraestructuras actúan tanto como emisores como causas directas del
+        cambio climático. Los centros urbanos crean dos categorías principales
+        de impactos sobre el ciclo del carbono y el sistema climático,
+        concretamente cambios relacionados con la emisión de aerosoles, GEI y
+        residuos sólidos, y cambios relacionados con el uso del suelo". (ONU
+        Hábitat. 2011: 16)
+      </p>
+      <h4>
+        ¿Qué se puede hacer frente a este escenario? ¿Qué podemos hacer como
+        habitantes de las ciudades? ¿Qué propone esta guía?
+      </h4>
+      <p>
+        La huella de carbono se define como el conjunto de emisiones de gases de
+        efecto invernadero producidas, directa o indirectamente, por personas,
+        organizaciones, productos, eventos o regiones geográficas, en términos
+        de CO2 equivalentes. La medición de esta huella es una herramienta de
+        gestión útil para conocer las conductas o acciones que contribuyen a
+        aumentar nuestras emisiones, cómo podemos mejorarlas y realizar un uso
+        más eficiente de los recursos.
+      </p>
+      <p>
+        Hoy las huellas de nuestras ciudades son enormes y para reducirlas es
+        clave avanzar en su planificación, innovar en el sector energético,
+        efectuar cambios en los modelos constructivos y en los medios que
+        utilizaremos para transportarnos. Además, como ciudadanos y ciudadanas
+        podemos y debemos contribuir a reducir el impacto del cambio climático
+      </p>
+      <p>
+        En ese sentido, la{" "}
+        <strong>Agenda 2030 para el Desarrollo Sostenible</strong> tiene como
+        finalidad erradicar la pobreza y el hambre en todo el mundo de aquí al
+        2030, combatir las desigualdades, construir sociedades pacíficas, justas
+        e inclusivas, proteger los derechos humanos, promover la igualdad entre
+        los géneros, a la vez se garantiza la protección duradera del planeta y
+        sus recursos naturales.
+      </p>
+      <p>
+        La provincia del Neuquén, en línea con esta Agenda, promueve medidas
+        para la protección del ambiente y la sostenibilidad en el territorio,
+        favoreciendo a que todas las personas gocen de paz y prosperidad.
+      </p>
+      <p>
+        Desde COPADE trabajamos de manera transversal con la Agenda, en acciones
+        vinculadas a las ciudades y sus áreas urbanas. En efecto, contribuimos a
+        la planificación de ciudades más sostenibles a través de los planes de
+        ordenamiento territorial y de las estrategias de urbanismo táctico en
+        los que participación ciudadana resulta fundamental. Proponemos pensar y
+        crear nuevas urbanidades y nuevas ruralidades, más sostenibles (ver
+        informe{" "}
+        <a
+          href="https://www.copade.gob.ar/bases-acciones-y-estrategias/"
+          rel="noreferrer"
+          target="_blank"
+        >
+          <strong>Bases, Acciones y Estrategias</strong>
+        </a>
+        )
+      </p>
+      <p>
+        Asímismo, desde el área de Ciencia, Tecnología e Innovación procuramos
+        brindar soluciones a los desafíos cotidianos que cada comunidad tiene y,
+        al mismo tiempo, apuntalar las buenas prácticas que se van generando en
+        el territorio para lograr multiplicarlas.
+      </p>
+      <p>
+        En búsqueda de fortalecer esta línea de trabajo, se ha enviado a la
+        Legislatura de la Provincia del Neuquén un proyecto de Ley para crear la{" "}
+        <strong>Agencia Neuquina de Innovación para el Desarrollo</strong> (ver
+        proyecto descargable{" "}
+        <a
+          href="https://www.copade.gob.ar/agencia-neuquina-de-innovacion-para-el-desarrollo/"
+          rel="noreferrer"
+          target="_blank"
+        >
+          aquí
+        </a>
+        . <em>Nota: dicho proyecto de Ley ya fue aprobado.</em>)
+      </p>
+      <p>
+        Las diversas estrategias gubernamentales son insuficientes. Es clave que
+        desde la ciudadanía se reflexione sobre esta problemática y se cambien
+        hábitos y costumbres de manera comprometida con el territorio que
+        habitamos. Las comunidades junto a sus gobiernos podrán detener este
+        proceso y, de manera colaborativa, cumplir con el mandato ético y legal
+        intergeneracional de un ambiente mejor y más sano para las genercaiones
+        venideras (artículo 41 de la Constitución Nacional).
+      </p>
+      <p>
+        Esta guía está dirigida a la ciudadanía con el objetivo de promover la
+        reducción del impacto negativo que ejercemos sobre el entorno que
+        habitamos y mejorar nuestra calidad de vida. Creemos que pequeños
+        cambios y transformaciones desde lo individual, acompañados de políticas
+        públicas pertinentes, promueven la paz y la felicidad y contribuyen a
+        alcanzar el desarrollo integral individual y colectivo de manera
+        sostenible.
+      </p>
+      <h4>¿Cómo construimos esta Guía?</h4>
+      <p>
+        La guía tuvo un proceso extenso y continuo de síntesis progresivas, con
+        la colaboración de diversos profesionales y organismos del Estado
+        Provincial. Está dirigida a la ciudadanía en sentido amplio, y pretende
+        ser un disparador inicial de diversas accciones transformadoras en
+        dirección a la sostenibilidad.
+      </p>
+      <p>
+        Consideramos que las dimensiones y enfoques para un documento como el
+        presente pueden ser casi infinitos, cada puerta abierta conduce a
+        múltiples caminos para construir mejores futuros.
+      </p>
+      <p>
+        Nos hemos centrado en la ciudadanía, en nuestras acciones como seres
+        humanos, producto de nuestros patrones de comportamiento, como centro
+        fundamental para comenzar este camino.
+      </p>
+      <p>
+        Este punto inicial, en conjunto con nuestras acciones desde el espacio
+        que habitamos, nuestra casa, constituye un sitio perfecto para estimular
+        procesos transformadores. La solidaridad es la dimensión que conecta
+        nuestra perspectiva individual con aquella otra, la comunitaria. Nos
+        hace partícipes activos del cambio. ¿Te sumás?
+      </p>
+      <h4>¿Desafíos?</h4>
+      <p>
+        A lo largo de esta guía encontrarás algunos pequeños desafíos para introducir
+        cambios concretos en tu vida cotidiana, que nos permitan en conjunto comprometernos
+        en profundidad y promover nuevos hábitos.
+      </p>
+    </div>
+  );
+}
 
 export default Chapter1;
