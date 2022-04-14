1969d49c4c7d3dd0111c67feb416003a8cc684cb
LichiBruno <lichiii@pm.me>
2022-04-14 00:55:45 -0300

    added chapter 1 to page 16

--- src/pages/Page1/Chapter1.js
+++ src/pages/Page1/Chapter1.js
@@ -7,14 +7,31 @@ function Chapter1() {
       <div className='Chapters'>
         <Recycle style={{ 
           color: 'rgb(238, 86, 3)',
+          // uncomment the lines below to center icon
           // position: 'relative',
           // left: '50%',
           width: '2em',
           height: '2em'
       }}/>
         <h3>Contexto general</h3>
-        <h4>Contexto general</h4>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p>En este capítulo revisamos los aspectos generales del contexto global que nos atraviesa, estableciendo los interrogantes esenciales que motivan y fundamentan la creación de esta guía. Así, proponemos identificar las problemáticas como paso prioritario para iniciar una reflexión necesaria, definir objetivos y ejecutar los grandes o pequeños cambios que se requieran.</p>
+        <h3>El cambio climático</h3>
+        <h4>¿Qué relación tiene con las ciudades y la ciudadanía?</h4>
+        <p>La Organización de las Naciones Unidas (ONU) estima que en la actualidad más de la mitad de la población en el mundo vive en ciudades y que esta cifra podría alcanzar los dos tercios para el año 2030.</p>
+        <p>En América Latina el porcentaje es aún mayor, dónde el 81% de la población vive en áreas urbanas (Banco Mudnial, 2020). En estas ciudades se consume una gran cantidad de energía y se producen aproximadamente el 70% de las emisiones de gases de efecto invernadero derivadas de la energía, que absorben el calor y provocan el calentamiento de la Tierra.</p>
+        <h4>¿Cómo afecta el cambio climático a las ciudades?</h4>
+        <p>El informe "Las ciudades y el cambio climático. Orientaciones para políticas" (ONU hábitat: 2011) advierte que si bien los riesgos del cambio climático varían en función de las particularidades de las ciudades, es posible establecer algunas características comunes:</p>
+        <ol>
+          <li>El cambio climático no afecta a toda la población de una ciudad por igual, por el contrario, aspectos como género, edad y situación económica intervienen en el grado de vulnerabilidad de las personas. Es decir, que el impacto del cambio climático reforzará las desigualdades existentes.</li>
+          <li>La falta de planificación urbama y las características constructivas de las ciudades podrían ser un limitante a la adaptación ed la infraestructura urbana, con el consecuente riesgo para las personas que en ellas habitan y sus bienes. Por ello, en la planificación de las ciudades deben tenerse en cuenta las demandas actuales y futuras de recursos. También habrá de identificarse los grupos humanos más vulnerables al cambio climático</li>
+          <li>Los impactos del cambio climático como la dificultad en la presentación de servicios básicos, la afectación de bienes y servicios de los ecosistemas pueden ser de largo plazo.</li>
+        </ol>
+        <p>"El análisis de cómo los centros urbanos contribuyen al cambio climático requiere comprender cómo el transporte, los sistemas de calefacción y refrigeración, las industrias y otras actividades urbanas e infraestructuras actúan tanto como emisores como causas directas del cambio climático. Los centros urbanos crean dos categorías principales de impactos sobre el ciclo del carbono y el sistema climático, concretamente cambios relacionados con la emisión de aerosoles, GEI y residuos sólidos, y cambios relacionados con el uso del suelo". (ONU Hábitat. 2011: 16)</p>
+        <h4>¿Qué se puede hacer frente a este escenario? ¿Qué podemos hacer como habitantes de las ciudades? ¿Qué propone esta guía?</h4>
+        <p>La huella de carbono se define como el conjunto de emisiones de gases de efecto invernadero producidas, directa o indirectamente, por personas, organizaciones, productos, eventos o regiones geográficas, en términos de CO2 equivalentes. La medición de esta huella es una herramienta de gestión útil para conocer las conductas o acciones que contribuyen a aumentar nuestras emisiones, cómo podemos mejorarlas y realizar un uso más eficiente de los recursos.</p>
+        <p>Hoy las huellas de nuestras ciudades son enormes y para reducirlas es clave avanzar en su planificación, innovar en el sector energético, efectuar cambios en los modelos constructivos y en los medios que utilizaremos para transportarnos. Además, como ciudadanos y ciudadanas podemos y debemos contribuir a reducir el impacto del cambio climático</p>
+        <p>En ese sentido, la <strong>Agenda 2030 para el Desarrollo Sostenible</strong> tiene como finalidad erradicar la pobreza y el hambre en todo el mundo de aquí al 2030, combatir las desigualdades, construir sociedades pacíficas, justas e inclusivas, proteger los derechos humanos, promover la igualdad entre los géneros, a la vez se garantiza la protección duradera del planeta y sus recursos naturales.</p>
+        <p>La provincia del Neuquén, en línea con esta Agenda, promueve medidas para la protección del ambiente y la sostenibilidad en el territorio, favoreciendo a que todas las personas gocen de paz y prosperidad.</p>
       </div>
     );
   }
