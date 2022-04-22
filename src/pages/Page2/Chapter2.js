803105d2abdf1001c4eba97945c53c6ebda56092
LichiBruno <lichiii@pm.me>
2022-04-22 01:24:32 -0300

    chapter 2 complete

--- src/pages/Page2/Chapter2.js
+++ src/pages/Page2/Chapter2.js
@@ -69,10 +69,12 @@ function Chapter2() {
       <p><span style={yellow}><strong>REPARAR:</strong></span> Arreglar un producto si se daña para darle una segunda oportunidad. Reparar los objetos nos ayuda a cuidar nuestro planeta.</p>
       <p><span style={yellow}><strong>RECICLAR:</strong></span> Es el conjunto de operaciones de recolección, clasificación y "tratamiento" de residuos que permiten reintroducirlos en un nuevo ciclo de vida. Ejemplo: se utiliza la separación de residuos en origen para facilitar su reciclado, como el cartón, el papel, el vidrio y todo material que pueda procesarse nuevamente sin perder sus características funcionales.</p>
       <p>A esto podemos agregar la "R" que invita a <strong>REPENSAR</strong> nuestros hábitos y modo de vida, especialmente con respecto a nuestras necedidades básicas.</p>
+      <div style={neonYellow}>
       <h3 style={textGlow}>DESAFÍO 7 días</h3>
       <p>Te proponemos aprovechar tu tiempo libre realizando tareas de reuso, reparación o reciclado.</p>
       <p>La idea podría comenzar, por ejemplo, evaluando tu vestimenta y realizando remiendos creativos con hilos de bordar. Esta actividad se puede compartir en familia, incorporando a niñas y niños en la tarea, dibujando con bordados y remendando la ropa.</p>
       <p>Otra posibilidad es transformar algún objeto de uso y necesidad; también podemos utilizar materiales de descarte (cartones, latas, plásticos) para generar nuevos objetos, por ejemplo estantes fabricados con planchas superpuestas de cartón. Las posibilidades son infinitas y, si la experiencia es compartida, seguramente surjan nuevas ideas en el proceso.</p>
+      </div>
       <h3 style={yellow}>¿Qué es la obsolescencia programada?</h3>
       <p>Se entiende por obsolescencia programada a la disminución, de manera intencionada, de la vida útil de un producto por parte de sus fabricantes para incrementar su consumo. Esto genera que consumamos productos con una vida cada vez más corta generando un gran problema ambiental para el planeta.</p>
       <div style={neonYellow}>
@@ -80,9 +82,11 @@ function Chapter2() {
       </div>
       <p style={{paddingTop: "1em"}}>Resulta clave combatir la obsolescencia programada, que supone también un perjucio económico para quienes consumen, al tener que renovar sus productos con mayor frecuencia.</p>
       <p>Las y los consumidores debemos exigir productos responsables con el ambiente y comprar de manera consciente. Ya hay mucha gente trabajando en una economía circular, pero es necesario continuar innovando para conseguir un planeta más sostenible.</p>
+      <div style={neonYellow}>
       <h3 style={textGlow}>DESAFÍO 7 días</h3>
       <p>Durante esta semana te proponemos tratar de hacer pequeños trueques con personas de tu barrio o entorno de trabajo (tus amistades, tu familia, tu barrio y tu trabajo). Podés comenzar proponiendo cambiar con alguien algún objeto que esté en tu casa al que no le des uso (celular, cafetera, una juguera que compraste y está en un rincón). Tal vez puedas cambiarla por algo que te sea de utilidad y que posea otra persona.</p>
       <p>Es un pequeño cambio de lugar para los objetos que utilizamos, pero implica también deshacernos de objetos que no necesitamos sin descartarlos en la "basura". Circular en nuestro entorno, sin recurrir a nuevos elementos de mercado.</p>
+      </div>
       <h3 style={yellow}>Residuos</h3>
       <p>El Ministerio de Ambiente de la Nación define al residuo como "todo elemento, material, objeto o sustancia que, como consecuencia de los procesos de consumo y desarrollo de actividades humanas, es desechado o abandonado. Los residuos sólidos urbanos (RSU) son análogos a los denominados domiciliarios y pueden ser de origen residencial, urbano, comercial, asistencial, sanitario, industrial o institucional, con excepción de aquellos que se encuentren regulados por normas específicas."</p>
       <p>Así la cantidad de RSU que generan quienes habitan un área, refleja el consumo y producción de una sociedad y repercute no solo en el ambiente sino también en la salud humana.</p>
@@ -123,9 +127,54 @@ function Chapter2() {
           <li><strong>COLABORAR CON LA GENERACIÓN DE NUEVOS PUESTOS DE TRABAJO:</strong> El reciclaje genera nuevas oportunidades laborales para personas que se encargan de producir objetos con ellos.</li>
       </ul>
       <p>Esta diferencia de procesos hace que los materiales secos queden disponibles para la reutilización y el reciclaje y los húmedos se conviertan en basura, para luego de ser tratados como corresponde. Entonces, si repasamos este proceso, podemos concluir en que no generamos la misma cantidad de basura si separamos los residuos como corresponde, ya que en una parte va a ser reciclada y reutilizada y otra parte va a ser destinada como basura. Por esto, debemos hacer hincapié en la importancia que tiene la separación de residuos desde el hogar donde generamos todo tipo de desechos.</p>
+      <div style={neonYellow}>
       <h3 style={textGlow}>DESAFÍO 21 días</h3>
       <p>Durante 21 días te proponemos organizarte para hacer una separación de residuos en tu casa. De esta forma, no solo podrás tomar conciencia de tus hábitos de consumo, sino que además, si dispones de macetas o espacio en tu patio, podés comenzar a elaborar compost para tu jardín o huerta.</p>
       <p>Podrás ver que hay muchas partes de los vegetales que utilizamos para cocinar que descartamos y que pueden ser destinadas para elaborar otras recetas (por ejemplo las pencas de acelga, la cáscara de papa o de banana). Aquí inician nuevas alternativas...</p>
+      <p>¡En 21 días de cambios sostenidos lograremos ir forjando nuevos hábitos!</p>
+      </div>
+      <h3 style={yellow}>REUTILIZAR Y RECICLAR PARA USO COTIDIANO</h3>
+      <p>A continuación, se listan algunas prácticas que contribuyen al cuidado del ambiente y que solo implican pequeños cambios en nuestra forma de consumo.</p>
+      <p><strong>Reducir, reutilizar y reciclar el plástico.</strong></p>
+      <p>El plástico debido a su manera sencilla y económica de producción, a su practicidad y versatilidad de usos se produce a grandes escalas, cambiando nuestras vidas, pero también saturando el entorno en el que vivimos. Según el Programa de las Naciones Unidas para el Medio Ambiente (PNUMA), la población mundial desecha todos los años el equivalente de su propio peso en plásticos. El mismo informe advierte que el 9% se recicla, el 12% se incinera y el 79% restante sigue dando vueltas.</p>
+      <p>Se cuentan por millones las toneladas que terminan en mares, ríos, bosques, afectando la vida silvestre y generando daños en la salud de la población mundial.</p>
+      <p>Acá te presentamos algunas de las acciones que podemos hacer para contribuir a mejorar el contexto:</p>
+      <ul>
+        <li>Dejar de usar botellas o vasos de plástico. Si es necesario consumir bebidas embotelladas, elige aquellas que sean de vidrio y retornables.</li>
+        <li>Dejar de usar sorbetes, cubiertos plásticos, platos descartables.</li>
+        <li>Dejar de usar toallitas húmedas en los casos que podamos reemplazarlas.</li>
+        <li>Dejar de pedir bolsas plásticas cuando quello que transportaremos ya se encuentra empaquetado, como por ejemplo cuando pedimos comida.</li>
+        <li>Utilizar para hacer las compras eco-bolsas que puedan ser de tela, plástico reciclado o papel.</li>
+        <li>Utilizar recipientes de vidrio o de metal para guardar y proteger la comida en vez de bolsas plásticas o films.</li>
+        <li>Reutilizar frascos de vidrio para guardar alimentos.</li>
+      </ul>
+      <iframe width="100%" height="100%" src="https://www.youtube.com/embed/iJgBH2cTENQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <h3 style={yellow}>COMPRAR DE MANERA INTELIGENTE:</h3>
+      <h4 style={yellow}>Ventas a granel, productos sin envases</h4>
+      <p>Comprar a granel significa que llevamos a la tienda nuestros propios envases, ya sean bolsas, frascos o <em>tuppers</em>, y colocamos directamente allí los productos que deseamos comprar para no utilizar recipientes ni bolsas de plástico. Es una modalidad de adquirir los diversos productos que conlleva diversos beneficios, así como también modifica hábitos de consumo que poseemos en pos de la protección de nuestro ambiente:</p>
+      <ul>
+        <li>Reducimos el uso de plástico (envases y etiquetas) y con ello el precio de los productos adquiridos.</li>
+        <li>Podemos comprar cantidades adecuadas a nuestro consumo.</li>
+        <li>Podemos comprar productos locales, ya que es más sencillo adquirirlos de este modo.</li>
+      </ul>
+      <p>Existen almacenes y tiendas en donde podemos adquirir productos a granel, ya sea artículos de limpieza, como alimentos y otros.</p>
+      <p>Los comercios que ofrecen estos servicios y posibilidades pueden realizar compras mayoristas para el expendio de los mismos. Además, la diversidad de productos puede alcanzar distintos rubros, desde alimentos a productos de limpieza o cosmética, entre otros.</p>
+      <p>Así, se reduce la huella de carbono por parte de todas las personas que participan de estos circuitos. Se pueden acompañar estas acciones con beneficios y protecciones estatales, que permitan mayor beneficio a los comercios, como también premiar a las y los clientes que accedan (premios, descuentos, regalos).</p>
+      <h3 style={yellow}>6 CONSEJOS ÚTILES</h3>
+      <ol>
+        <li>Siempre llevá bolsas de tela para guardar tus compras.</li>
+        <li>Comprá productos frescos a granel. Evitá envasados.</li>
+        <li>Comprá tan solo lo que necesites.</li>
+        <li>Priorizá el consumo de productos locales y de temporada.</li>
+        <li>Planeá tus comidas con anticipación. Así reducirás desperdicio.</li>
+        <li>¿Y qué tal si iniciás la producción de tus propios alimentos? Nuestra guía "Cultivar en la ciudad" te puede ser de utilidad.</li>
+      </ol>
+      <div style={neonYellow}>
+      <h3 style={textGlow}>DESAFÍO 15 días</h3>
+      <p>Durante los próximos quince días te proponemos descubrir los comercios a granel que pueden encontrarse en tu barrio. Para conocer aquellos productos que puedan comprarse allí según tus hábitos de consumo.</p>
+      <p>Podés empezar limpiando algunos envases vacíos, para luego hacer tus compras. Con ello también podrás ver los productos que consumís y las cantidades necesarias en tu vida cotidiana.</p>
+      <p>También podés dejar en tu bolso, cartera o en un lugar cercano, una eco-bolsa (de tela por ejemplo) a la que recurras a la hora de ir a hacer las compras, de modo que no tengas que utilizar bolsas plásticas cada vez.</p>
+      </div>
     </div>
   );
 }
