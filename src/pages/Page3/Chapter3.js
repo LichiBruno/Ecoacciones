9ee3668cf346f674a0d58cdaae1e7f675919cff8
LichiBruno <lichiii@pm.me>
2022-04-22 23:57:14 -0300

    added license

--- src/pages/Page3/Chapter3.js
+++ src/pages/Page3/Chapter3.js
@@ -14,26 +14,27 @@ function Chapter3() {
           height: "2em",
         }}
       />
-      <h3>si</h3>
+      <h3 style={green}>Sostenibilidad y el espacio en casa</h3>
+      <p>En el presente apartado te proponemos algunos ejemplos útiles a la hora de pensar los espacios que habitamos, para que sean lo más eficiente posible, tanto en el manejo de la energía como en el del agua y en los materiales que se utilizan. Analizamos cómo puede diseñarse nuestra vivienda para lograr confort y sostenibilidad.</p>
     </div>
   );
 }
 
-// const green = {
-//   color: "rgbrgb(11, 126, 25)"
-// };
+const green = {
+  color: "rgb(11, 126, 25)"
+};
 
-// const neonYellow = {
-//   boxSizing: "1px",
-//   border: "none",
-//   borderRadius: "20px",
-//   padding: "1em 0.5em 0.2em 1em",
-//   boxShadow: "0px 5px 26px 8px rgbrgb(11, 126, 25)"
-// }
+const neonGreen = {
+  boxSizing: "1px",
+  border: "none",
+  borderRadius: "20px",
+  padding: "1em 0.5em 0.2em 1em",
+  boxShadow: "0px 5px 26px 8px rgb(11, 126, 25)"
+}
 
-// const textGlow = {
-//   textShadow: "0 0 10px rgbrgb(11, 126, 25), 0 0 20px rgbrgb(11, 126, 25), 0 0 30px rgbrgb(11, 126, 25), 0 0 40px rgbrgb(11, 126, 25), 0 0 50px rgbrgb(11, 126, 25)",
-//   color: "inherit"
-// }
+const textGlow = {
+  textShadow: "0 0 10px rgb(11, 126, 25), 0 0 20px rgb(11, 126, 25), 0 0 30px rgb(11, 126, 25), 0 0 40px rgb(11, 126, 25), 0 0 50px rgb(11, 126, 25)",
+  color: "inherit"
+}
 
 export default Chapter3;
